4fdc14a877ee1f3bdef4494a7c7f7163da4b2090
jinny <eujin.mstory@gmail.com>
2024-08-18 23:39:16 +0900

    feat: colors

---
+++ Row_and_Column/lib/const/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const colors = [
+  Colors.red,
+  Colors.orange,
+  Colors.yellow,
+  Colors.green,
+];
+
--- Row_and_Column/lib/screen/home_screen.dart
+++ Row_and_Column/lib/screen/home_screen.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:row_and_column/const/colors.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 50.0,
+              width: 50.0,
+              color: Colors.red,
+            ),
+            Container(
+              height: 50.0,
+              width: 50.0,
+              color: Colors.orange,
+            ),
+            Container(
+              height: 50.0,
+              width: 50.0,
+              color: Colors.yellow,
+            ),
+
+
+          ],
+        ),
+      )
+    );
   }
 }
 
